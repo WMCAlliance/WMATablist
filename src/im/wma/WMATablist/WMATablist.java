5ee58c0780eaa481366b4ca1ecbbb044a5e65bc6
Matt Gajownik <git@wizardcm.com>
2013-05-06 19:03:44 +1000

    Begin the code

--- src/im/wma/WMATablist/WMATablist.java
+++ src/im/wma/WMATablist/WMATablist.java
@@ -1,6 +1,8 @@
 /* Main class */
 package im.wma.WMATablist;
 
+import me.bubbleguj.supertab.SuperTab;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -10,5 +12,14 @@ import org.bukkit.ChatColor;
 
 
 public class WMATablist extends JavaPlugin {
-  
+	@EventHandler (priority = EventPriority.HIGHEST)
+	public void onJoin(PlayerJoinEvent event) {
+		
+		ArrayList<String> messages = new ArrayList<String>();
+		messages.add("&1Worldwide");
+		messages.add("&4Minecraft");
+		messages.add("&2Alliance");
+		
+		SuperTab.setTab(messages);
+	}
 }
