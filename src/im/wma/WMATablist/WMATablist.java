eba674b513df4d2373a1eab4d733a8d42d0fad59
Matt Gajownik <git@wizardcm.com>
2013-05-06 18:09:20 +1000

    Add some basic imports to atart with

--- src/im/wma/WMATablist/WMATablist.java
+++ src/im/wma/WMATablist/WMATablist.java
@@ -1,6 +1,14 @@
 /* Main class */
 package im.wma.WMATablist;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+
+
 public class WMATablist extends JavaPlugin {
   
 }
