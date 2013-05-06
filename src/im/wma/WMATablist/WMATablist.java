1bf451e029cd427c474a39ddc92dfedf5c975f52
Matt Gajownik <git@wizardcm.com>
2013-05-06 20:27:28 +1000

    Initial proper compilation stuff (will do a gitignore soon).

--- src/im/wma/WMATablist/WMATablist.java
+++ src/im/wma/WMATablist/WMATablist.java
@@ -10,9 +10,17 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerJoinEvent;
+import java.util.ArrayList;
 
-
-public class WMATablist extends JavaPlugin {
+public class WMATablist extends JavaPlugin implements Listener {
+	public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+	
 	@EventHandler (priority = EventPriority.HIGHEST)
 	public void onJoin(PlayerJoinEvent event) {
 		
